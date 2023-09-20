1733f19d64e8e0d99f695446596527fe1748ec5f
Chris Ware <c.p.ware@hotmail.com>
2023-09-19 22:45:37 -0700

    Login + Register Page

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import styles from "./LoginPage.module.css";
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <form action=''></form>
+    </div>
+  );
+};
+export default Login;
